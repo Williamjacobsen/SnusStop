53c7632164316c1dfddee51506e14ef9bef33528
Williamjacobsen <71521465+Williamjacobsen@users.noreply.github.com>
2023-01-22 10:30:35 +0100

    fadein/fadeout menu bg

--- frontend/utils/BurgerMenu.js
+++ frontend/utils/BurgerMenu.js
@@ -1,18 +1,49 @@
-import { View, Animated } from "react-native";
-import { useState, useEffect, useRef } from "react";
+import { View } from "react-native";
+import { useState, useEffect } from "react";
 import { theme, styles } from "./../utils/style.js";
 
 export default function BurgerMenu({ menu }) {
+  const [menuOpacity, setMenuOpacity] = useState(0);
+  const [menuHeight, setMenuHeight] = useState(88);
+
+  useEffect(() => {
+    if (menu) {
+      setMenuOpacity(0);
+      var counter = 1;
+      var fadein = setInterval(() => {
+        if (counter == 11) {
+          clearInterval(fadein);
+        } else {
+          counter++;
+          setMenuOpacity((prev) => prev + 0.1);
+        }
+      }, 25);
+    }
+    if (!menu) {
+      var counter = 1;
+      var fadeout = setInterval(() => {
+        if (counter == 11) {
+          clearInterval(fadeout);
+        } else {
+          counter++;
+          setMenuOpacity((prev) => prev - 0.1);
+        }
+      }, 25);
+    }
+  }, [menu]);
+
+  useEffect(() => console.log(`menuOpacity : ${menuOpacity}`), [menuOpacity]);
+
   return (
-    <Animated.View
+    <View
       style={{
         position: "absolute",
-        top: "12%", // flex 1, 1, 10 = flex 1 == 12%
-        height: "88%",
+        top: "12%",
+        height: `${menuHeight}%`,
         width: "100%",
         backgroundColor: theme.lightBlue,
-        opacity: 1,
+        opacity: menuOpacity,
       }}
-    ></Animated.View>
+    ></View>
   );
 }
