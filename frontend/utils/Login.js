d622f70fc8ce379de9915e945fb2911b3c8596eb
Williamjacobsen <71521465+Williamjacobsen@users.noreply.github.com>
2023-01-25 22:29:58 +0100

    login popup ui
    lags on animation, cuz defaultSource REEEEEEE

--- frontend/utils/Login.js
+++ frontend/utils/Login.js
@@ -1,6 +1,8 @@
-import { TouchableOpacity, View, Text, Image } from "react-native";
+import { TouchableOpacity, View, Text, Image, TextInput } from "react-native";
 import { useState, useEffect } from "react";
 import { theme, styles } from "./../utils/style.js";
+const closeBtn = require("./../assets/close.png");
+const userIcon = require("./../assets/user.png");
 
 export default function Login({ state, setState }) {
   const [menuOpacity, setMenuOpacity] = useState(0);
@@ -18,7 +20,7 @@ export default function Login({ state, setState }) {
           counter++;
           setMenuOpacity((prev) => prev + 0.1);
         }
-      }, 10);
+      }, 25);
     }
     if (!state) {
       var counter = 1;
@@ -30,7 +32,7 @@ export default function Login({ state, setState }) {
           counter++;
           setMenuOpacity((prev) => prev - 0.1);
         }
-      }, 10);
+      }, 25);
     }
   }, [state]);
 
@@ -53,7 +55,7 @@ export default function Login({ state, setState }) {
       <View
         style={{
           width: "80%",
-          height: "70%",
+          height: "72.5%",
           backgroundColor: theme.lightGray,
           borderRadius: 10,
           opacity: menuOpacity,
@@ -63,29 +65,121 @@ export default function Login({ state, setState }) {
           style={{
             position: "relative",
             left: "80%",
-            top: "5%",
+            top: "2.5%",
             zIndex: 11,
             opacity: state ? 1 : 0,
+            backgroundColor: "#fff",
+            borderRadius: "50%",
+            padding: 10,
+            width: 50,
           }}
           onPress={() => {
             setState((prev) => !prev);
           }}
         >
           <Image
-            source={require("./../assets/close.png")}
-            style={{ height: 40, width: 40 }}
-          ></Image>
+            style={{ height: 30, width: 30 }}
+            source={closeBtn}
+            defaultSource={closeBtn} // stop flickering, but lags animation
+          />
         </TouchableOpacity>
         <Text
           style={{
-            fontSize: 50,
+            fontSize: 40,
             textAlign: "center",
-            fontWeight: "600",
+            fontWeight: "700",
             color: theme.lightBlue,
           }}
         >
-          Login
+          LOGIN
         </Text>
+        <View
+          style={{
+            backgroundColor: "black",
+            height: 2,
+            opacity: 0.5,
+            width: "80%",
+            position: "relative",
+            left: "10%",
+            marginTop: 10,
+          }}
+        ></View>
+        <View style={{ height: "80%", alignItems: "center" }}>
+          <Image
+            style={{
+              height: "15%",
+              width: "23%",
+              marginTop: "7.5%",
+              opacity: 0.5,
+            }}
+            source={userIcon}
+            defaultSource={userIcon}
+          />
+          <TextInput
+            style={[styles.input, { height: state ? 50 : 0 }]}
+            placeholder="Brugernavn..."
+          />
+          <TextInput
+            style={[styles.input, { height: state ? 50 : 0 }]}
+            placeholder="Adgangskode..."
+          />
+          <TouchableOpacity
+            style={{ width: "100%", position: "relative", left: "10%" }}
+          >
+            <View
+              style={[
+                styles.input,
+                {
+                  height: state ? 50 : 0,
+                  marginTop: "15%",
+                  justifyContent: "center",
+                },
+              ]}
+            >
+              <Text
+                style={{
+                  fontSize: 20,
+                  opacity: 0.75,
+                  paddingLeft: "10%",
+                }}
+              >
+                Log ind med
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{ width: "100%", position: "relative", left: "10%" }}
+          >
+            <View
+              style={[
+                styles.input,
+                {
+                  height: state ? 50 : 0,
+                  marginTop: "5%",
+                  justifyContent: "center",
+                },
+              ]}
+            >
+              <Text
+                style={{
+                  fontSize: 20,
+                  opacity: 0.75,
+                  paddingLeft: "10%",
+                }}
+              >
+                Log ind med
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              styles.btn,
+              { height: "10%", width: "80%", marginTop: "7.5%" },
+            ]}
+          >
+            <Text style={{ fontSize: 20 }}>Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   };
