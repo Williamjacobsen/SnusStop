4c14d47feb044da108e391d6f77306a6d7160941
Williamjacobsen <71521465+Williamjacobsen@users.noreply.github.com>
2023-02-14 23:43:36 +0100

    main page ui

---
+++ frontend/pages/MainPage.js
@@ -0,0 +1,219 @@
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  Platform,
+} from "react-native";
+import React from "react";
+import { theme, styles } from "./../utils/style.js";
+import BurgerMenu from "./../utils/BurgerMenu.js";
+
+export default function MainPage() {
+  const [menu, setMenu] = React.useState(false);
+  const [home, setHome] = React.useState(true);
+  const [statistics, setStatistics] = React.useState(false);
+
+  const [antalSnusIDag, setAntalSnusIDag] = React.useState(0);
+
+  React.useEffect(() => {
+    console.log(
+      `\nmenu : ${menu}\nhome : ${home}\nstatistics : ${statistics}\n`
+    );
+  }, [menu, home, statistics]);
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={styles.navbar}>
+        <View
+          style={{
+            height: "100%",
+            flex: 1,
+            justifyContent: Platform.OS !== "ios" ? "center" : null,
+            paddingLeft: 20,
+          }}
+        >
+          <TouchableOpacity
+            style={[styles.btn, { opacity: home ? 0.75 : 1 }]}
+            onPress={() => {
+              setHome(true);
+              setStatistics(false);
+              setMenu(false);
+            }}
+          >
+            <Text style={{ fontSize: 20 }}>Home</Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            height: "100%",
+            flex: 1,
+            justifyContent: Platform.OS !== "ios" ? "center" : null,
+            paddingLeft: 10,
+          }}
+        >
+          <TouchableOpacity
+            style={[styles.btn, { opacity: statistics ? 0.75 : 1 }]}
+            onPress={() => {
+              setHome(false);
+              setStatistics(true);
+              setMenu(false);
+            }}
+          >
+            <Text style={{ fontSize: 20 }}>Statistics</Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            height: "100%",
+            flex: 1.5,
+            justifyContent: "center",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              setMenu((prev) => !prev);
+            }}
+          >
+            <Image
+              source={require("./../assets/burger_menu.png")}
+              style={{
+                height: "80%",
+                width: 40,
+                position: "relative",
+                left: "60%",
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <BurgerMenu menu={menu} />
+      <View style={{ flex: 10, backgroundColor: "#fff" }}>
+        {home ? (
+          <>
+            <View
+              style={{
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{
+                  marginTop: 25,
+                  fontSize: 30,
+                  textAlign: "center",
+                  fontWeight: "700",
+                  color: theme.lightBlue,
+                }}
+              >
+                STREAK
+              </Text>
+              <Text
+                style={{
+                  fontSize: 25,
+                  textAlign: "center",
+                  fontWeight: "600",
+                  opacity: 0.5,
+                }}
+              >
+                28 Dage
+              </Text>
+              <Text
+                style={{
+                  marginTop: 25,
+                  fontSize: 30,
+                  textAlign: "center",
+                  fontWeight: "700",
+                  color: theme.lightBlue,
+                }}
+              >
+                PENGE SPARET
+              </Text>
+              <Text
+                style={{
+                  fontSize: 25,
+                  textAlign: "center",
+                  fontWeight: "600",
+                  opacity: 0.5,
+                }}
+              >
+                399.- Kr
+              </Text>
+              <Text
+                style={{
+                  marginTop: 25,
+                  fontSize: 30,
+                  textAlign: "center",
+                  fontWeight: "700",
+                  color: theme.lightBlue,
+                }}
+              >
+                NEDSAT ANTAL SNUS
+              </Text>
+              <Text
+                style={{
+                  fontSize: 25,
+                  textAlign: "center",
+                  fontWeight: "600",
+                  opacity: 0.5,
+                }}
+              >
+                63 Stk. Snus
+              </Text>
+              <Text
+                style={{
+                  marginTop: 25,
+                  fontSize: 30,
+                  textAlign: "center",
+                  fontWeight: "700",
+                  color: theme.lightBlue,
+                }}
+              >
+                ANTAL SNUS I DAG?
+              </Text>
+              <Text
+                style={{
+                  fontSize: 35,
+                  textAlign: "center",
+                  fontWeight: "500",
+                  opacity: 0.5,
+                  marginTop: 10,
+                }}
+              >
+                {antalSnusIDag}
+              </Text>
+              <TouchableOpacity
+                style={[
+                  styles.btn,
+                  { height: 50, width: 50, marginTop: -45, marginLeft: -125 },
+                ]}
+                onPress={() => {
+                  if (antalSnusIDag > 0) {
+                    setAntalSnusIDag((prev) => prev - 1);
+                  }
+                }}
+              >
+                <Text style={{ fontSize: 25 }}>-1</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[
+                  styles.btn,
+                  { height: 50, width: 50, marginTop: -50, marginLeft: 125 },
+                ]}
+                onPress={() => setAntalSnusIDag((prev) => prev + 1)}
+              >
+                <Text style={{ fontSize: 25 }}>+1</Text>
+              </TouchableOpacity>
+            </View>
+          </>
+        ) : (
+          <></>
+        )}
+      </View>
+    </View>
+  );
+}
