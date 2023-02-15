a8b0bdeab06c43059403b2280c812c935de2a1be
Williamjacobsen <71521465+Williamjacobsen@users.noreply.github.com>
2023-02-15 15:09:24 +0100

    refactoring - more readable and dry

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mysql = require("mysql2"); // https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
+const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -28,16 +28,17 @@ const db = mysql.createConnection({
 const createAccount = (req, res, userExists) => {
   if (!userExists && req.body.id) {
     db.query(
-      `INSERT INTO accounts (google_id, email, verified_email, name, given_name, family_name, picture, locale) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+      `INSERT INTO accounts (google_id, email, verified_email, name, given_name, family_name, picture, locale, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
       [
-        req.body.id,
-        req.body.email,
-        req.body.verified_email ? 1 : 0,
+        req.body?.id ? req.body.id : null,
+        req.body?.email ? req.body.email : null,
+        req.body?.verified_email ? 1 : 0,
         req.body.name,
-        req.body.given_name,
-        req.body.family_name,
-        req.body.picture,
-        req.body.locale,
+        req.body?.given_name ? req.body.given_name : null,
+        req.body?.family_name ? req.body.family_name : null,
+        req.body?.picture ? req.body?.picture : null,
+        req.body?.locale ? req.body.locale : null,
+        req.body?.password ? req.body.password : null,
       ],
       (err, result) => {
         if (err) {
@@ -51,27 +52,6 @@ const createAccount = (req, res, userExists) => {
     );
   }
 };
-
-app.post("/Google", (req, res) => {
-  let userExists = false;
-  if (req.body.id) {
-    db.query(
-      `SELECT * FROM accounts WHERE google_id = ${req.body.id}`,
-      (err, result) => {
-        if (err) {
-          console.error(err);
-          res.send({ status: "failure", message: null });
-        } else if (result[0]) {
-          userExists = true;
-          console.log(`Account already exists with google_id ${req.body.id}`);
-          res.send({ status: "success", message: "loggedIn" });
-        } else {
-          createAccount(req, res, userExists);
-        }
-      }
-    );
-  }
-});
 
 const insertUserData = (req, res) => {
   db.query(
@@ -95,19 +75,46 @@ const insertUserData = (req, res) => {
   );
 };
 
-app.post("/UserData", (req, res) => {
-  if (req.body.googleID) {
-    db.query(
-      `SELECT * FROM accounts WHERE google_id = ${req.body.googleID}`,
-      (err, result) => {
-        if (err) {
-          console.error(err);
-          res.send({ status: "failure", message: null });
-        } else {
-          insertUserData(req, res);
-        }
+const doesAccountExist = (googleID) => {
+  db.query(
+    `SELECT * FROM accounts WHERE google_id = ${googleID}`,
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.send({ status: "failure", message: null });
+        return false;
+      } else {
+        return true;
       }
+    }
+  );
+};
+
+app.post("/Google", (req, res) => {
+  let userExists = false;
+  if (req.body?.id) {
+    if (doesAccountExist(req.body.id)) {
+      userExists = true;
+      console.log(`Account already exists with google_id ${req.body.id}`);
+      res.send({ status: "success", message: "loggedIn" });
+      return;
+    }
+    createAccount(req, res, userExists);
+  }
+});
+
+app.post("/UserData", (req, res) => {
+  if (!req.body?.googleID) {
+    return;
+  }
+
+  if (doesAccountExist(req.body.googleID)) {
+    insertUserData(req, res);
+  } else {
+    console.log(
+      `Account does not exist where google_id = ${req.body.googleID}`
     );
+    res.send({ status: "failure", message: null });
   }
 });
 
